87503e07379f61f0386acb59f939ae4ce6bd5014
Esref Ozdemir <esref.ozdemir27@gmail.com>
2018-03-05 19:19:39 +0300

    Implement conjunctive query

--- include/util.hpp
+++ include/util.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
-#include <vector>
 #include <string>
+#include <vector>
 
 std::vector<std::string> split(std::string& str, const std::string& delimeters);
 
@@ -13,4 +13,24 @@ bool one_of(InputIterator begin, InputIterator end, const T& value) {
         }
     }
     return false;
+};
+
+template <typename InputIterator1, typename InputIterator2,
+          typename OutputIterator>
+OutputIterator intersect(InputIterator1 first_begin, InputIterator1 first_end,
+                         InputIterator2 second_begin, InputIterator2 second_end,
+                         OutputIterator out) {
+    while (first_begin != first_end && second_begin != second_end) {
+        if (*first_begin == *second_begin) {
+            *out = *first_begin;
+            ++first_begin;
+            ++second_begin;
+            ++out;
+        } else if (*first_begin < *second_begin) {
+            ++first_begin;
+        } else {
+            ++second_begin;
+        }
+    }
+    return out;
 };
--- src/query_processor.cpp
+++ src/query_processor.cpp
@@ -1,20 +1,11 @@
 #include "query_processor.hpp"
+#include "util.hpp"
+#include <algorithm>
 
 QueryProcessor::QueryProcessor() {}
 
 QueryProcessor::QueryProcessor(const term_id_map& dict, const id_pos_map& index)
     : m_dict(dict), m_index(index) {}
-
-std::vector<size_t>
-QueryProcessor::conjunctive_query(const std::vector<std::string>& words) const {
-    return {};
-}
-
-std::vector<size_t>
-QueryProcessor::proximity_query(const std::vector<std::string>& words,
-                                const std::vector<size_t>& dists) const {
-    return {};
-}
 
 const term_id_map& QueryProcessor::dict() const { return this->m_dict; }
 
@@ -23,3 +14,55 @@ void QueryProcessor::dict(const term_id_map& dict) { this->m_dict = dict; }
 const id_pos_map& QueryProcessor::index() const { return this->m_index; }
 
 void QueryProcessor::index(const id_pos_map& index) { this->m_index = index; }
+
+std::vector<size_t>
+QueryProcessor::conjunctive_query(const std::vector<std::string>& words) const {
+    std::vector<std::vector<size_t>> posting_lists;
+    for (const auto& word : words) {
+        if (m_dict.find(word) == m_dict.end()) {
+            return std::vector<size_t>();
+        }
+        size_t id = m_dict.at(word);
+        const auto& doc_vec = m_index.at(id);
+
+        std::vector<size_t> postings(doc_vec.size());
+        std::transform(doc_vec.begin(), doc_vec.end(), postings.begin(),
+                       [](const auto& doc_pair) { return doc_pair.first; });
+
+        posting_lists.push_back(postings);
+    }
+
+    if (posting_lists.empty()) {
+        return std::vector<size_t>();
+    } else if (posting_lists.size() == 1) {
+        return posting_lists[0];
+    }
+
+    std::sort(posting_lists.begin(), posting_lists.end(),
+              [](const auto& first, const auto& second) {
+                  return first.size() < second.size();
+              });
+
+    std::vector<size_t> result;
+    intersect(posting_lists[0].begin(), posting_lists[0].end(),
+              posting_lists[1].begin(), posting_lists[1].end(),
+              std::back_inserter(result));
+
+    std::vector<size_t> tmp;
+    for (size_t i = 2; i < posting_lists.size(); ++i) {
+        intersect(result.begin(), result.end(),
+                  posting_lists[i].begin(), posting_lists[i].end(),
+                  std::back_inserter(tmp));
+
+        result = tmp;
+        tmp.clear();
+    }
+
+    return result;
+}
+
+std::vector<size_t>
+QueryProcessor::proximity_query(const std::vector<std::string>& words,
+                                const std::vector<size_t>& dists) const {
+    return {};
+}
